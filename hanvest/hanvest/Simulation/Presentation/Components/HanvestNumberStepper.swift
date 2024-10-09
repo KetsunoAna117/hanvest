4f2a80b772aa0c2e32d116d91072d075a2e8a72a
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-09 09:52:56 +0700

    Create SharedUI Folder

---
+++ hanvest/hanvest/Simulation/Presentation/Components/HanvestNumberStepper.swift
@@ -0,0 +1,18 @@
+//
+//  HanvestNumberStepper.swift
+//  hanvest
+//
+//  Created by Hans Arthur Cupiterson on 09/10/24.
+//
+
+import SwiftUI
+
+struct HanvestNumberStepper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanvestNumberStepper()
+}
