58818178815e9e996b047dff3dd596a1a8ea04a1
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-09 10:15:58 +0700

    Create Hanvest Button Default Class

---
+++ hanvest/hanvest/SharedUI/Button/HanvestButtonDefault.swift
@@ -0,0 +1,24 @@
+//
+//  HanvestButtonDefault.swift
+//  hanvest
+//
+//  Created by Hans Arthur Cupiterson on 09/10/24.
+//
+
+import SwiftUI
+
+struct HanvestButtonDefault: View {
+    var 
+    
+    var title: String
+    var image: Image?
+    var action: () -> Void
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanvestButtonDefault()
+}
