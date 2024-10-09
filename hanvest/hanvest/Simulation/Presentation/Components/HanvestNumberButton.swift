29f8c3a420f04f5494269c31d4ff53819ba091d0
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-09 10:31:38 +0700

    New File HanvestNumberButton

---
+++ hanvest/hanvest/Simulation/Presentation/Components/HanvestNumberButton.swift
@@ -0,0 +1,34 @@
+//
+//  HanvestNumberButton.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 09/10/24.
+//
+
+import SwiftUI
+
+enum ButtonState {
+    case current
+    case done
+    case next
+}
+
+enum IsPressed {
+    case yes
+    case no
+}
+
+struct HanvestNumberButton: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            <#code#>
+        }
+
+    }
+}
+
+#Preview {
+    HanvestNumberButton()
+}
