6051c5e2f923fde7f114ce328611e444bd9d7c1c
Bryan Vernanda <91310099+13ry4nV3rn4nd4@users.noreply.github.com>
2024-10-10 12:33:21 +0700

    Han 94 fe create module number button that can be used to access certain learning module (#9)
    * New File HanvestNumberButton
    
    * Developing Button Module Number, not done yet.
    
    * Button Module Number is almost done but with bad animation.
    
    * Han-94-[FE]-Create Module Number Button that can be used to access certain learning module is DONE !!
    
    * Give better structure for view builder
    
    * Update code based on figma, I forgot on the last commit :')
    
    * Removing default value to be using if let

---
+++ hanvest/hanvest/Material/Presentation/Components/ModuleNumber/Views/HanvestModuleNumberButton.swift
@@ -0,0 +1,95 @@
+//
+//  HanvestNumberButton.swift
+//  hanvest
+//
+//  Created by Bryan Vernanda on 09/10/24.
+//
+
+import SwiftUI
+
+struct HanvestModuleNumberButton: View {
+    // Constants
+    let SHADOW_OFFSET: CGFloat = 5
+    
+    @State var state: HanvestModuleNumberDefaultState = .pressed
+    
+    // Styling variable
+    var style: HanvestModuleNumberDefaultStyle = .current
+    
+    // Button content
+    var number: Int?
+    var image: Image?
+    var action: () -> Void
+    
+    @ViewBuilder
+    var imageOrNumberViewBuilder: some View {
+        if let image = image {
+            image
+        } else if let number = number {
+            Text("\(number)")
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(style.backgroundColor)
+                .shadow(
+                    color: getPressedStatus() ? .clear : style.shadowColor,
+                    radius: getPressedStatus() ? 0 : 0, x: 0, y: getPressedStatus() ? 0 : SHADOW_OFFSET
+                )
+                
+            imageOrNumberViewBuilder
+                .fontWeight(.bold) // will need to be adjusted to new fonts
+                .font(.system(size: 34)) // will need to be adjusted to new fonts
+                .foregroundStyle(.mineShaft50)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.3), value: self.state)
+        }
+        .frame(maxWidth: 80, maxHeight: 80)
+        .offset(y: getPressedStatus() ? SHADOW_OFFSET : 0)
+        .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.3), value: self.state)
+        .onTapGesture {
+             action()
+         }
+        .onLongPressGesture(minimumDuration: 0.1, pressing: { isPressing in
+             withAnimation {
+                 if isPressing {
+                     self.state = .pressed
+                 }
+                 else if self.style == .current {
+                     self.state = .unpressed
+                 }
+             }
+         }, perform: {
+             if self.style == .current {
+                 action()
+             }
+         })
+        .onAppear {
+            if self.style == .current {
+                self.state = .unpressed
+            }
+        }
+    }
+    
+    func getPressedStatus() -> Bool {
+        return state == .pressed
+    }
+}
+
+
+#Preview {
+    // To test for checkmark:
+    // change "number" param to "image" with the content is
+    // Image(systemName: "checkmark")
+    // and change style to .done
+    
+    VStack {
+        HanvestModuleNumberButton(
+            style: .current,
+            number: 1, action: {
+                print("Hello World!")
+            })
+    }
+    .padding(.horizontal, 16)
+}
