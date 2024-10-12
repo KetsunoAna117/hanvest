776f5debe52a66c605d473821300a0eb9372e3d0
Benedick Wijayaputra <ibenwp1@gmail.com>
2024-10-13 02:17:51 +0700

    feat: Change Slider from a built in Slider into a Custom Slider

--- hanvest/hanvest/SharedUI/Slider/HanvestSlider.swift
+++ hanvest/hanvest/SharedUI/Slider/HanvestSlider.swift
@@ -8,28 +8,53 @@
 import SwiftUI
 
 struct HanvestSlider: View {
-    @Binding var value: Double
-    var minimum: Double
-    var maximum: Double
+    @Binding var value: Int
+    var minimum: Int
+    var maximum: Int
+    
+    func calculateProgressPercentage() -> CGFloat {
+        guard maximum > minimum else { return 0 }
+        
+        let percentage = CGFloat(value - minimum) / CGFloat(maximum - minimum)
+        return min(max(percentage, 0), 1)
+    }
     
     var body: some View {
-        VStack {
-            Slider(value: $value, in: minimum...maximum)
-                .frame(width: 280, height: 4)
-                .tint(.seagull400)
-                .padding()
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(height: 8)
+                        .foregroundStyle(.fillSecondary)
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(width: geometry.size.width * calculateProgressPercentage() , height: 8)
+                        .foregroundStyle(.seagull400)
+                    
+                    Circle()
+                        .frame(width: 28, height: 28)
+                        .foregroundColor(.mineShaft50)
+                        .position(x: geometry.size.width * calculateProgressPercentage(), y: 14)
+                        .gesture(DragGesture()
+                            .onChanged { value in
+                                let dragPercentage = value.location.x / geometry.size.width
+                                let newValue = Int(dragPercentage * CGFloat(maximum - minimum)) + minimum
+                                self.value = min(max(newValue, minimum), maximum)
+                            }
+                        )
+                }
+            }
         }
+        .frame(height: 28)
     }
 }
 
 #Preview {
-    @Previewable @State var value : Double = 30
-
+    @Previewable @State var value: Int = 50
     VStack {
         HanvestSlider(value: $value, minimum: 0, maximum: 100)
-                    .padding()
+            .padding(24)
         
         Text("Value: \(Int(value))")
     }
-    .padding(.horizontal, 16)
 }
