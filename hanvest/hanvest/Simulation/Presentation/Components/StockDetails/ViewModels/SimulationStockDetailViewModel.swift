00182e7b9e1d58364bb3015e16979ecee60ea006
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-14 10:23:33 +0700

    refactor: Update display logic in Simulation Stock Detail View

--- hanvest/hanvest/Simulation/Presentation/Components/StockDetails/ViewModels/SimulationStockDetailViewModel.swift
+++ hanvest/hanvest/Simulation/Presentation/Components/StockDetails/ViewModels/SimulationStockDetailViewModel.swift
@@ -8,12 +8,5 @@
 import Foundation
 
 class SimulationStockDetailViewModel: ObservableObject {
-    @Inject var getStockByID: GetStockInformationByID
-    
-    @Published var simulationStock: SimulationStockEntity
-    
-    init(){
-        simulationStock = getStockByID.execute(id: "1")
-    }
-    
+
 }
--- hanvest/hanvest/Simulation/Presentation/Components/StockDetails/Views/SimulationStockDetailsView.swift
+++ hanvest/hanvest/Simulation/Presentation/Components/StockDetails/Views/SimulationStockDetailsView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct SimulationStockDetailsView: View {
-    var selectedStockID: String
-    
-    @StateObject private var viewmodel = SimulationStockDetailViewModel()
+    var selectedStock: SimulationStockEntity
     
     @State private var initialPrice: Int = 0
     @State private var currentPrice: Int = 0
@@ -18,15 +16,15 @@ struct SimulationStockDetailsView: View {
     var body: some View {
         VStack {
             StockHeaderInformationView(
-                stockCodeName: viewmodel.simulationStock.stockName,
-                stockName: viewmodel.simulationStock.stockName,
+                stockCodeName: selectedStock.stockIDName,
+                stockName: selectedStock.stockName,
                 initialPrice: $initialPrice,
                 currentPrice: $currentPrice
             )
         }
-        .onReceive(viewmodel.$simulationStock) { result in
-            initialPrice = result.stockPrice.startIndex
-            currentPrice = result.stockPrice.endIndex
+        .onAppear(){
+            initialPrice = selectedStock.stockPrice.first?.stockPrice ?? 0
+            currentPrice = selectedStock.stockPrice.last?.stockPrice ?? 0
         }
 
     }
@@ -34,6 +32,17 @@ struct SimulationStockDetailsView: View {
 
 #Preview {
     SimulationStockDetailsView(
-        selectedStockID: "BBRI"
+        selectedStock: SimulationStockEntity(
+            stockIDName: "BBRI",
+            stockName: "Pt. Bank Rakyat Indonesia Tbk.",
+            stockImageName: "bri",
+            stockDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rhoncus tellus nulla, ut blandit neque rutrum in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sem lectus, semper a libero ut, congue sodales sem. Ut convallis volutpat ipsum quis maximus.",
+            stockPrice: [
+                SimulationStockPriceEntity(stockPrice: 5000, time: HanvestDateFormatter.stringToDate("2024-10-10 20:23:00")),
+                SimulationStockPriceEntity(stockPrice: 6000, time: HanvestDateFormatter.stringToDate("2024-10-11 20:33:00")),
+                SimulationStockPriceEntity(stockPrice: 4000, time: HanvestDateFormatter.stringToDate("2024-10-11 20:43:00")),
+                SimulationStockPriceEntity(stockPrice: 5000, time: HanvestDateFormatter.stringToDate("2024-10-11 20:53:00"))
+            ]
+        )
     )
 }
