a09eb884c3cdbcbee728feb09da93d0c4a44efbb
Benedick Wijayaputra <ibenwp1@gmail.com>
2024-10-14 10:22:29 +0700

    feat: Create a Module Description Component

---
+++ hanvest/hanvest/Material/Presentation/Components/ModuleDescription/HanvestModuleDescription.swift
@@ -0,0 +1,18 @@
+//
+//  HanvestModuleDescription.swift
+//  hanvest
+//
+//  Created by Benedick Wijayaputra on 14/10/24.
+//
+
+import SwiftUI
+
+struct HanvestModuleDescription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanvestModuleDescription()
+}
