1ee0ae8a95fffdf753ecf72e63680e4cdb9173f4
Benedick Wijayaputra <ibenwp1@gmail.com>
2024-10-14 10:48:22 +0700

    feat: Buying Card view and viewmodel template

---
+++ hanvest/hanvest/Simulation/Presentation/Components/BuyingCard/View/SimulationBuyingCard.swift
@@ -0,0 +1,20 @@
+//
+//  SimulationBuyingCard.swift
+//  hanvest
+//
+//  Created by Benedick Wijayaputra on 14/10/24.
+//
+
+import SwiftUI
+
+struct SimulationBuyingCard: View {
+    @ObservedObject var viewmodel: BuyingStockDataViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+//    SimulationBuyingCard()
+}
---
+++ hanvest/hanvest/Simulation/Presentation/Components/BuyingCard/ViewModel/BuyingStockDataViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BuyingStockDataViewModel.swift
+//  hanvest
+//
+//  Created by Benedick Wijayaputra on 14/10/24.
+//
+
+import SwiftUI
+
+class BuyingStockDataViewModel: ObservableObject{
+    
+}
+
