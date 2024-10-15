88ca3e3c82d21a801ab8e7e0b82edaa4e848fd4a
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-15 12:55:02 +0700

    feat: Create NewsButton and ViewModels

---
+++ hanvest/hanvest/Simulation/Presentation/Components/NewsButton/Views/HanvestNewsButton.swift
@@ -0,0 +1,19 @@
+//
+//  HanvestNewsButton.swift
+//  hanvest
+//
+//  Created by Hans Arthur Cupiterson on 15/10/24.
+//
+
+import SwiftUI
+
+struct HanvestNewsButton: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanvestNewsButton()
+}
