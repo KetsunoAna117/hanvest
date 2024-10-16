5d6ac1552bab1e009f76b6fdbe772c21e7ad22aa
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-10-16 10:52:09 +0700

    feat: Added Confirmation Row Components

--- hanvest/hanvest/Simulation/Presentation/Components/BuyingConfirmation/SimulationBuyingConfirmationCard.swift
+++ hanvest/hanvest/Simulation/Presentation/Components/BuyingConfirmation/SimulationBuyingConfirmationCard.swift
@@ -11,17 +11,19 @@ struct SimulationBuyingConfirmationCard: View {
     var viewModel: BuyingStockDataConfirmationViewModel
     var cancelAction: () -> ()
     var confirmAction: () -> ()
+    
     var body: some View {
         HanvestCardBackground {
             VStack(alignment: .center, spacing: 24){
                 Text("Preview Buy Order").font(.nunito(.body))
                 VStack(alignment: .leading, spacing: 12){
-                    rowDes(title: "Stock", value: viewModel.stock.uppercased())
-                    rowDes(title: "Price", value: "\(viewModel.priceStock)")
-                    rowDes(title: "Fee", value: "\(viewModel.fee)")
-                    rowDes(title: "Lot", value: "\(viewModel.lot)")
-                    rowDes(title: "Amount", value: "\(viewModel.priceAmount)")
-                    }
+                    ConfirmationRow(title: "Stock", value: viewModel.stock.uppercased())
+                    ConfirmationRow(title: "Price", value: "\(HanvestPriceFormatter.formatIntToIDR(viewModel.priceStock))")
+                    ConfirmationRow(title: "Fee", value: "\(HanvestPriceFormatter.formatIntToIDR(viewModel.fee))")
+                    ConfirmationRow(title: "Lot", value: "\(viewModel.lot)")
+                    ConfirmationRow(title: "Amount", value: "\(HanvestPriceFormatter.formatIntToIDR(viewModel.priceAmount))")
+                }
+                
                 HStack{
                     HanvestButtonDefault(
                         size: .medium,
@@ -38,35 +40,25 @@ struct SimulationBuyingConfirmationCard: View {
                         action: confirmAction
                     )
                 }
-            }.padding(16)
+            }
+            .padding(16)
         }
-        }
-    func rowDes(title: String, value: String) -> some View {
-        HStack(spacing: 24) {
-            Text(title)
-                .font(.nunito(.body))
-                .frame(width: 120, alignment: .topLeading)
-            Text(value)
-                .font(.nunito(.body))
-        }
-        .frame(maxWidth: .infinity, alignment: .topLeading)
     }
-    }
+}
+
 #Preview {
-    SimulationBuyingConfirmationCard(viewModel: BuyingStockDataConfirmationViewModel(
-        stock: "bbca",
-        priceStock: 5000,
-        lot: 1,
-        fee: 500
-        ),
-        cancelAction: {
-            print("Cancel button pressed")
-        },
-        
-        confirmAction: {
-            print("Confirm button pressed")
-        }
-    )
+    SimulationBuyingConfirmationCard(
+        viewModel:
+            BuyingStockDataConfirmationViewModel(
+                stock: "BBCA",
+                priceStock: 5000,
+                lot: 1,
+                fee: 500
+            ), cancelAction: {
+                print("Cancel button pressed")
+            }, confirmAction: {
+                print("Confirm button pressed")
+            })
     .padding(.horizontal, 16)
-    }
+}
 
