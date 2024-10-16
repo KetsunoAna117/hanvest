ff36f6dedcd63a0fd059002922d0923c8c91d679
Christian Gunawan <cg404cgunawan@gmail.com>
2024-10-16 13:07:11 +0700

    feat: create hanvest glossary Word Row

---
+++ hanvest/hanvest/Glossary/Presentation/Components/HanvestGlossaryWordLetterRow.swift
@@ -0,0 +1,18 @@
+//
+//  HanvestGlossaryWordLetterRow.swift
+//  hanvest
+//
+//  Created by Christian Gunawan on 16/10/24.
+//
+
+import SwiftUI
+
+struct HanvestGlossaryWordLetterRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanvestGlossaryWordLetterRow()
+}
